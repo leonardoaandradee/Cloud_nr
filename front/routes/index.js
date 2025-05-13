1e690b5e807e6c220874ab6a06a312d75b47f226
Eduardo Cadiz <181763279+educadiz@users.noreply.github.com>
2025-05-13 16:53:30 +0000

    Ajustes em layout

--- front/routes/index.js
+++ front/routes/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Pizza Show' });
+    res.render('pages/index', { title: 'Pizza Show' });
 });
 
 router.get('/produtos', (req, res) => {
-    res.render('produtos/produtos', { title: 'Produtos - Pizza Show' });
+    res.render('pages/produtos/produtos', { title: 'Produtos - Pizza Show' });
 });
 
 router.get('/clientes', (req, res) => {
-    res.render('clientes/clientes', { title: 'Clientes - Pizza Show' });
+    res.render('pages/clientes/clientes', { title: 'Clientes - Pizza Show' });
 });
 
 router.get('/pedidos', (req, res) => {
-    res.render('pedidos/pedidos', { title: 'Pedidos - Pizza Show' });
+    res.render('pages/pedidos/pedidos', { title: 'Pedidos - Pizza Show' });
 });
 
 module.exports = router;
